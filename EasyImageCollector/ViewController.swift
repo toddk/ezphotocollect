69634d787dce39e08323fc6cb4c697098f635855
Todd Krokowski <toddkrokowski@gmail.com>
2019-09-09 19:54:33 -0400

    Initial Commit

---
+++ EasyImageCollector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyImageCollector
+//
+//  Created by Todd Krokowski on 9/9/19.
+//  Copyright © 2019 Todd Krokowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
